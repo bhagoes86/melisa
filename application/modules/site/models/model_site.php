99692791aeb08300ef136ed9326a8f86f671c33d
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-04-10 06:08:32 +0700

    quiz fixer

--- application/modules/site/models/model_site.php
+++ application/modules/site/models/model_site.php
@@ -8,14 +8,15 @@ class Model_site extends CI_Model {
     function __construct() {
         parent::__construct();
     }
-    
-    function show_record(){
+
+    function show_record() {
         $this->db->select('*');
         $this->db->from('table_site');
         return $this->db->get();
     }
+
     //untuk semua update yang berbentuk text
-    function data_update($header,$menu1,$menu2,$menu3,$caption,$footer,$bgheader,$bgfooter,$tgpicture1,$tgpicture2,$tgpicture3){
+    function data_update($header, $menu1, $menu2, $menu3, $caption, $footer, $bgheader, $bgfooter, $tgpicture1, $tgpicture2, $tgpicture3) {
         $this->db->set('header', $header);
         $this->db->set('menu1', $menu1);
         $this->db->set('menu2', $menu2);
@@ -29,71 +30,79 @@ class Model_site extends CI_Model {
         $this->db->set('tgpicture3', $tgpicture3);
         $this->db->update('table_site');
     }
-    
-    function header_update($header){
+
+    function header_update($header) {
         $this->db->set('header', $header);
         $this->db->update('table_site');
     }
-    
-    function bgheader_update($bgheader){
+
+    function bgheader_update($bgheader) {
         $this->db->set('bgheader', $bgheader);
         $this->db->update('table_site');
     }
-    
-    function footer_update($footer){
+
+    function footer_update($footer) {
         $this->db->set('footer', $footer);
         $this->db->update('table_site');
     }
-    
-    function bgfooter_update($bgfooter){
+
+    function bgfooter_update($bgfooter) {
         $this->db->set('bgfooter', $bgfooter);
         $this->db->update('table_site');
     }
-    
-    function caption_update($caption){
+
+    function caption_update($caption) {
         $this->db->set('caption', $caption);
         $this->db->update('table_site');
     }
-    
-    function menu1_update($menu1){
+
+    function menu1_update($menu1) {
         $this->db->set('menu1', $menu1);
         $this->db->update('table_site');
     }
-    
-    function menu2_update($menu2){
+
+    function menu2_update($menu2) {
         $this->db->set('menu2', $menu2);
         $this->db->update('table_site');
     }
-    
-    function menu3_update($menu3){
+
+    function menu3_update($menu3) {
         $this->db->set('menu3', $menu3);
         $this->db->update('table_site');
     }
-    
-    function tgpicture1_update($tgpicture1){
+
+    function tgpicture1_update($tgpicture1) {
         $this->db->set('tgpicture1', $tgpicture1);
         $this->db->update('table_site');
     }
-    function tgpicture2_update($tgpicture2){
+
+    function tgpicture2_update($tgpicture2) {
         $this->db->set('tgpicture2', $tgpicture2);
         $this->db->update('table_site');
     }
-    function tgpicture3_update($tgpicture3){
+
+    function tgpicture3_update($tgpicture3) {
         $this->db->set('tgpicture3', $tgpicture3);
         $this->db->update('table_site');
     }
-    
-    function update_smpicture($filename,$status){
-        if($status==1){
-                $this->db->set('smpicture1', $filename);
-                $this->db->update('table_site');
-            }elseif ($status==2) {
-                $this->db->set('smpicture2', $filename);
-                $this->db->update('table_site');
-            }else{
-                $this->db->set('smpicture3', $filename);
-                $this->db->update('table_site');
-         }
+
+    function update_smpicture($filename, $status) {
+        if ($status == 1) {
+            $this->db->set('smpicture1', $filename);
+            $this->db->update('table_site');
+        } elseif ($status == 2) {
+            $this->db->set('smpicture2', $filename);
+            $this->db->update('table_site');
+        } else {
+            $this->db->set('smpicture3', $filename);
+            $this->db->update('table_site');
+        }
+    }
+
+    function select_header_text() {
+        $this->db->select('*');
+        $this->db->from('table_site');
+        return $this->db->get;
     }
 
 }
