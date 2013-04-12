dcb10a5553546455bee4dd9f3e5d6d40486d7a2f
ridwanfs <ridwanbejo@gmail.com>
2013-04-13 00:11:43 +0700

    text kuis di course

--- application/modules/course/views/list_quiz.php
+++ application/modules/course/views/list_quiz.php
@@ -1,6 +1,12 @@
 <!-- <pre><?php //print_r($list_quiz); ?> </pre> -->
 <?php if ($list_quiz == null){
-    echo "<h3>Saat ini tidak ada kuis yang tersedia untuk kuliah ini ..</h3>";
+    ?>
+    
+    <p style="margin-top: 0px; padding-top: 0px;color: rgb(94,94,94);font-size: 14px;">
+        Saat ini tidak ada kuis yang tersedia untuk kuliah ini
+    </p>
+        
+    <?php
 } else {
     ?>
 
@@ -14,6 +20,8 @@
                 <td><?php echo $quiz->quiz_title; ?> - <?php echo $quiz->group_title ?></td>
                 <?php
                 if ($this->session->userdata('data_quiz') != '') {
+                    
+                    
                     if ($group_id == $quiz->id_group) {
                         ?>
                         <td class="center">
@@ -23,7 +31,7 @@
                     } else {
                         ?>
                         <td class="center">
-                            <a title="Maaf Sudah Ditutup" href="javascript:void(0)" id="btn-preview" data-id="<?php echo $quiz->id_group; ?>"><i class="icon-locked fg-color-red"></i></a>
+                            <a title="Maaf Sedang Ditutup" href="javascript:void(0)" id="btn-preview" data-id="<?php echo $quiz->id_group; ?>"><i class="icon-locked fg-color-red"></i></a>
                         </td>
                         <?php
                     }
@@ -41,11 +49,28 @@
                         }
 
                         else {
+                            
+                            $today = getdate();
+                            $waktu1 = date_parse($quiz->start_time);
+                            $waktu2 = date_parse($quiz->end_time);
+
+                            $temp_interval1 = strtotime($today['year'].'-'.$today['mon'].'-'.$today['mday'].' '.$today['hours'].':'.$today['minutes'].':00') - strtotime($waktu1['year'].'-'.$waktu1['month'].'-'.$waktu1['day'].' '.$waktu1['hour'].':'.$waktu1['minute'].':00');
+                            $temp_interval2 = strtotime($today['year'].'-'.$today['mon'].'-'.$today['mday'].' '.$today['hours'].':'.$today['minutes'].':00') - strtotime($waktu2['year'].'-'.$waktu2['month'].'-'.$waktu2['day'].' '.$waktu2['hour'].':'.$waktu2['minute'].':00');
+
+                            if ($temp_interval1 > 0 && $temp_interval2 < 0){
                             ?>
                             <td class="center">
                                 <a title="Mulai Evaluasi" href="<?php echo site_url('quiz/gate_avail_quiz/' . $quiz->id_group.'/'.$id_course); ?>" target='_blank' id="btn-preview" data-id="<?php echo $quiz->id_group; ?>"><i class="icon-book fg-color-black"></i></a>
                             </td>
                             <?php
+                            } else {
+                            ?>
+                            <td class="center">
+                                <a title="Maaf Sudah Ditutup" href="javascript:void(0)" id="btn-preview" data-id="<?php echo $quiz->id_group; ?>"><i class="icon-locked fg-color-red"></i></a>
+                            </td>
+
+                            <?php
+                            }
                         }
                 ?>
 
