a2e078d21f9424cf720772bd663f797b12300e32
toni haryanto <toha.samba@gmail.com>
2013-04-15 16:28:48 +0700

    update feed style

--- application/modules/feed/views/latest.php
+++ application/modules/feed/views/latest.php
@@ -1,16 +1,127 @@
 <div class="span9">
     <h2 style="padding-top: 0px;margin-top: 0px;"><?php echo $template['title']; ?></h2>
     <ul class="listview list-long image">
-        <li id="feed-video">
+        <li class="feed-document">
             <span class="feed-avatar">
                 <img src="http://localhost/melisa/application/modules/feed/img/avatar.jpg">
             </span>
             <div class="data">
                 <div class="user-description">
-                    <h4><a href="#">Toni Haryanto</a>
+                    <h4><a href="#">Toni Haryanto</a> mengunggah dokumen
                         <span class="date-meta">&middot; kemarin 09:11 &middot; publik</span>
                     </h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="image document-image">
+                    <div class="icon bg-color-green fg-color-white"><i class="icon-libreoffice"></i></div>
+                    <div class="description">
+                        <h4><a href="#">Toni Haryanto Code Playground</a></h4>
+                        Ut egestas magna, sed rutrum felis fermentum quis. Aliquam congue ultricies elit, sit amet tempor tortor tempor nec.
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="utils">
+                    <div class="toolbar place-left">
+                        <button title="like"><i class="icon-thumbs-up"></i> <span class="badge">100</span></button>
+                        <button title="comment"><i class="icon-comments"></i> <span class="badge">70</span></button>
+                        <button title="share"><i class="icon-share"></i> <span class="badge">8</span></button>
+                    </div>
+                    <div class="toolbar place-right">
+                        <button title="setting"><i class="icon-cog"></i></button>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </li>
+
+        <li class="feed-link">
+            <span class="feed-avatar">
+                <img src="http://localhost/melisa/application/modules/feed/img/avatar.jpg">
+            </span>
+            <div class="data">
+                <div class="user-description">
+                    <h4><a href="#">Toni Haryanto</a> memposting tautan
+                        <span class="date-meta">&middot; kemarin 09:11 &middot; publik</span>
+                    </h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet <a href="#">#tempor</a>, consectetur adipisicing elit. Mollitia maxime placeat magnam laborum ullam quas odit molestiae eligendi nesciunt unde at accusantium excepturi commodi facilis similique quasi inventore deserunt a.</p>
+                    <div class="hide">
+                        <p><a href="#"><strong>Lorem ipsum</strong></a> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="#">#Excepteur</a> sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam unde facere perspiciatis nulla commodi labore accusamus dolorum ipsum alias tenetur quaerat animi sit doloremque ducimus laboriosam corporis ullam pariatur harum!</p>
+                    </div>
+                    <a class="hide-link">selengkapnya..</a>
+                </div>
+                <div class="image link-image">
+                    <img src="http://localhost/melisa/application/modules/feed/img/avatar.jpg">
+                    <div class="description">
+                        <h4><a href="#">Toni Haryanto Code Playground</a></h4>
+                        Ut egestas magna, sed rutrum felis fermentum quis. Aliquam congue ultricies elit, sit amet tempor tortor tempor nec.
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="utils">
+                    <div class="toolbar">
+                        <button title="like"><i class="icon-thumbs-up"></i> <span class="badge">100</span></button>
+                        <button title="comment"><i class="icon-comments"></i> <span class="badge">70</span></button>
+                        <button title="share"><i class="icon-share"></i> <span class="badge">8</span></button>
+                    </div>
+                </div>
+            </div>
+        </li>
+
+        <li class="feed-photo">
+            <span class="feed-avatar">
+                <img src="http://localhost/melisa/application/modules/feed/img/avatar.jpg">
+            </span>
+            <div class="data">
+                <div class="user-description">
+                    <h4><a href="#">Toni Haryanto</a> mengunggah foto
+                        <span class="date-meta">&middot; kemarin 09:11 &middot; publik</span>
+                    </h4>
+                </div>
+                <div class="text">
+                    <p>Lorem ipsum dolor sit amet <a href="#">#tempor</a>, consectetur adipisicing elit. Mollitia maxime placeat magnam laborum ullam quas odit molestiae eligendi nesciunt unde at accusantium excepturi commodi facilis similique quasi inventore deserunt a.</p>
+                    <div class="hide">
+                        <p><a href="#"><strong>Lorem ipsum</strong></a> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <a href="#">#Excepteur</a> sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam unde facere perspiciatis nulla commodi labore accusamus dolorum ipsum alias tenetur quaerat animi sit doloremque ducimus laboriosam corporis ullam pariatur harum!</p>
+                    </div>
+                    <a class="hide-link">selengkapnya..</a>
+                </div>
+                <div class="image video-image">
+                    <img src="http://localhost/melisa/application/modules/feed/img/i_love_you.jpg">
+                    <div class="description">
+                        Ut egestas magna, sed rutrum felis fermentum quis. Aliquam congue ultricies elit, sit amet tempor tortor tempor nec.
+                    </div>
+                </div>
+                <div class="utils">
+                    <div class="toolbar">
+                        <button title="like"><i class="icon-thumbs-up"></i> <span class="badge">100</span></button>
+                        <button title="comment"><i class="icon-comments"></i> <span class="badge">70</span></button>
+                        <button title="share"><i class="icon-share"></i> <span class="badge">8</span></button>
+                    </div>
+                </div>
+            </div>
+        </li>
+
+        <li class="feed-video">
+            <span class="feed-avatar">
+                <img src="http://localhost/melisa/application/modules/feed/img/avatar.jpg">
+            </span>
+            <div class="data">
+                <div class="user-description">
+                    <h4><a href="#">Toni Haryanto</a> mengunggah video
+                        <span class="date-meta">&middot; kemarin 09:11 &middot; publik</span>
+                    </h4>
                 </div>
                 <div class="image video-image fg-color-white">
                     <img src="http://localhost/melisa/application/modules/feed/img/photoshop-tutorial.jpg">
@@ -28,5 +139,6 @@
                 </div>
             </div>
         </li>
+
     </ul>
 </div>
