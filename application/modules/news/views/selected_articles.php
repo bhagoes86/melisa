780664c10196c51a7b4984e97dddac645357f04e
Jimho Obing <jimho.obing@gmail.com>
2013-04-16 15:39:05 +0700

    viewer attachment

--- application/modules/news/views/selected_articles.php
+++ application/modules/news/views/selected_articles.php
@@ -1,13 +1,42 @@
-<div class="span3">
-    <img src="<?php echo base_url() . 'attachment/' . $news->header ?>" style="width: 100%;">
-</div>
 <div class="span6">
-    <div class="bg-color-blueDark" style="margin-bottom: 10px;">
-        <a class="fg-color-white">&nbsp;Title</a>
-    </div>
-    <p><?php echo $news->title ?></p>
-     <div class="bg-color-greenDark" style="margin-bottom: 10px;">
-        <a class="fg-color-white">&nbsp;Isi</a>
-    </div>
-    <p><?php echo $news->news ?></p>
+    <?php if ($news == NULL) { ?>
+    <?php } else { ?>
+        <img src="<?php echo base_url() . 'attachment/' . $news->header ?>" style="width: 100%;">
+    <?php } ?>
+    <?php if ($news == NULL) { ?>
+
+    <?php } else { ?>
+        <div class="bg-color-blueDark" style="margin-bottom: 10px;">
+            <a class="fg-color-white">&nbsp;Title</a>
+        </div>
+        <p><?php echo $news->title ?></p>
+    <?php } ?>
+
+    <?php if ($news == NULL) { ?>
+
+    <?php } else { ?>
+        <div class="bg-color-greenDark" style="margin-bottom: 10px;">
+            <a class="fg-color-white">&nbsp;Isi</a>
+        </div>
+        <p><?php echo $news->news ?></p>
+
+        <?php
+        if (($news->attachment_type == 2) || ($news->attachment_type == 3) || ($news->attachment_type == 4) ||($news->attachment_type == 7)) {
+            echo modules::run('news/youtube_view',$news->attachment);
+        }
+        
+        if($news->attachment_type == 1){
+            echo modules::run('news/document_view',$news->attachment);
+        }
+        
+        if($news->attachment_type == 0){
+            echo modules::run('news/video_view',$news->attachment,$news->ext);
+        }
+        
+        if($news->attachment_type == 6){
+            echo modules::run('news/soundcloud_view',$news->attachment);
+        }
+    }
+    ?>
+
 </div>
---
+++ application/modules/news/views/viewer_slideshare.php
@@ -0,0 +1,43 @@
+<script type="text/javascript" src="<?php echo base_url() ?>asset/slideshare/swfobject.js"></script>
+<script type="text/javascript">    
+    var flashMovie;
+
+    //Load the flash player. Properties for the player can be changed here.
+    function loadPlayer() {
+        //allowScriptAccess from other domains
+        var params = { allowScriptAccess: "always" };
+        var atts = { id: "player" };
+
+        //doc: The path of the file to be used
+        //startSlide: The number of the slide to start from
+        //rel: Whether to show a screen with related slideshows at the end or not. 0 means false and 1 is true..
+        var flashvars = { doc : "<?php print_r($presentation['3']) ?>", startSlide : 1, rel : 0 };
+
+        //Generate the embed SWF file
+        swfobject.embedSWF("http://static.slidesharecdn.com/swf/ssplayer2.swf", "player", "100%", "560", "8", null, flashvars, params, atts);
+
+        //Get a reference to the player
+        flashMovie = document.getElementById("player");
+    }
+
+    //Jump to the appropriate slide
+    function jumpTo(){
+        flashMovie.jumpTo(parseInt(document.getElementById("slidenumber").value));
+    }
+
+    //Update the slide number in the field for the same
+    function updateSlideNumber(){
+        document.getElementById("slidenumber").value = flashMovie.getCurrentSlide();
+    }
+    //Google Analytic
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-31205461-3']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
