d26aac01ddd1a3205cdca0898ddcd14f0a25f866
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-04-22 10:19:37 +0700

    course

--- application/modules/course/views/course.php
+++ application/modules/course/views/course.php
@@ -48,19 +48,27 @@
                             <div class="span12">
                                 <div class="span3">
                                     <h3 style="font-weight: bold;">Intisari Kuliah</h3>
-                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;"></p>                                    
+                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                    </p>                                    
                                 </div>
                                 <div class="span3">
                                     <h3 style="font-weight: bold;">Pemahaman Dasar</h3>
-                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;"></p>                                    
+                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                    </p>                                    
                                 </div>
                                 <div class="span3">
                                     <h3 style="font-weight: bold;">Akan Dipelajari</h3>
-                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;"></p>                                    
+                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                    </p>                                    
                                 </div>
                                 <div class="span3">
                                     <h3 style="font-weight: bold;">Pemateri Kuliah</h3>
-                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;"></p>                                    
+                                    <p style="text-align: justify;color: rgb(94,94,94);font-size: 14px;">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                    </p>                                    
                                 </div>
                             </div>
 
