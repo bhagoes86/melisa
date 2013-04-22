5cda314ebfc637bbfe1619e253643af4dff51a63
ridwanfs <ridwanbejo@gmail.com>
2013-04-22 13:14:58 +0700

    assignment

---
+++ application/modules/assignment/models/model_assignment.php
@@ -0,0 +1,67 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Model_assignment extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function select_all_assignment($user_id) {
+        $this->db->select('*');
+        $this->db->from('assignment_file');
+        $this->db->where('user_id', $user_id);
+        $this->db->where('deleted', 0);
+        $this->db->order_by('date_modified', 'desc');
+        return $this->db->get();
+    }
+    
+    function select_assignment_by_id($id_assignment){
+        $this->db->select('*');
+        $this->db->from('assignment_file');
+        $this->db->where('id_assignment', $id_assignment);
+        return $this->db->get();
+    }
+    
+    function insert_assignment($data) {
+        $this->db->insert('assignment_file', $data);
+        return $this->db->insert_id();
+    }
+    
+    function update_assignment($id_assignment, $data) {
+        $this->db->where('id_assignment', $id_assignment);
+        $this->db->update('assignment_file', $data);
+    }
+    
+    
+    function select_all_course($user_id){
+        $this->db->select('*');
+        $this->db->from('course');
+        $this->db->where('user_id', $user_id);
+        $this->db->order_by('date', 'desc');
+        return $this->db->get();
+    }
+    
+    function select_course_by_assignment_id($user_id, $assignment_id){
+        $this->db->select('*');
+        $this->db->from('course');
+        $this->db->join('assignment_file', 'course.id_course=assignment_file.course_id');
+        $this->db->where('assignment_file.user_id', $user_id);
+        $this->db->where('assignment_file.deleted', 0);
+        $this->db->where('assignment_file.course_id !=', 0);
+        $this->db->where('assignment_file.id_assignment', $assignment_id);
+        
+        
+        return $this->db->get();
+    }
+    
+    function check_course_by_assignment_id($course_id, $assignment_id) {
+        $this->db->select('*');
+        $this->db->from('assignment_file');
+        $this->db->where('course_id', $course_id);
+        $this->db->where('id_assignment', $assignment_id);
+        return $this->db->get();
+    }
+}
---
+++ application/modules/assignment/views/list_assignment.php
@@ -0,0 +1,239 @@
+
+<script type="text/javascript" src="<?php echo base_url(); ?>asset/metro/js/modern/accordion.js"></script>
+<a class="button bg-color-yellow" id="assignment-add-form"><i class="icon-plus"></i>Tambah Tugas</a>
+<a class="button bg-color-orange fg-color-white" id="assignment-my-quiz-result"><i class="icon-stats"></i>Nilai Saya</a>
+
+
+<hr />
+
+
+
+
+<?php if (count($list_assignment) > 0) {?>
+
+<table class="striped" id="my-table">
+    <thead>
+        <tr >
+            <th style="text-align:center"><b>Tugas</b></th>
+            <th style="text-align:center"><b>Hasil Tugas</b></th>
+            <th style="text-align:center"><b>Aktif</b></th>
+            <th style="text-align:center"><b>Aksi</b></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($list_assignment as $assignment): ?>
+            <tr style="margin-bottom:20px">
+                <td style="border: 1px solid white;vertical-align: top;background-color:rgba(0, 0, 0, 0.0666667);">
+                    <a style="color: #095b97;font-size: 18px;"><?php echo  character_limiter($assignment->title, 30); ?></a><br/>
+                    <p style="color: rgb(94,94,94);font-size: 13px;"><?php echo cut_text($assignment->description, 45) ?> ...</p>
+                </td>
+                
+                <td style="border: 1px solid white;vertical-align: top;background-color:rgba(0, 0, 0, 0.0666667);">
+                    <div id="list-quiz-result" style="width:480px;height:150px;overflow:scroll;overflow-x:hidden;margin:0px 5px 5px 5px;">
+                          <?php //echo modules::run('quiz/show_quiz_course', $quiz->id_quiz); ?>
+                          
+                    </div>
+                </td>
+                <td style="border: 1px solid white;vertical-align: middle;background-color:rgba(0, 0, 0, 0.0666667);">
+                    <?php 
+                    $date1 = date_create($assignment->start_time);
+                    $date2 = date_create($assignment->end_time);
+                    
+                    if ($date1 == $date2) {
+                        ?>
+                            <a title="aktif" href="javascript:void(0)"  data-id=""><i class="icon-checkbox-unchecked" fg-color-black"></i></a>
+                        <?php
+                    }
+                    else {
+                        ?>
+                            <a title="aktif" href="javascript:void(0)"  data-id=""><i class="icon-checkbox" fg-color-black"></i></a>
+                        <?php
+                    }
+                    ?>
+                </td>
+                <td style="width: 30px;border: 1px solid white;vertical-align: middle;background-color:rgba(0, 0, 0, 0.0666667);">
+                    <a title="preview" href="javascript:void(0)" id="btn-preview" data-id="<?php echo $assignment->id_assignment; ?>"><i class="icon-book fg-color-black"></i></a>
+                    <a title="edit" href="javascript:void(0)" id="btn-edit" data-id="<?php echo $assignment->id_assignment; ?>"><i class="icon-pencil fg-color-pink"></i></a>
+                    <a title="grup" href="javascript:void(0)" id="btn-group" data-id="<?php echo $assignment->id_assignment; ?>"><i class="icon-briefcase fg-color-black"></i></a>
+                    <a title="daftar kuliah" href="javascript:void(0)" id="btn-list-course" data-id="<?php echo $assignment->id_assignment; ?>"><i class="icon-list fg-color-black"></i></a>
+                    <a title="hapus" href="javascript:void(0)" id="btn-delete" data-id="<?php echo $assignment->id_assignment?>"><i class="icon-remove fg-color-red"></i></a>
+               </td>
+
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+        <?php } else { ?>
+            <tr>
+                <td><h2>Tidak ada tugas yang bisa dipakai....</h2></td>
+            </tr>
+        <?php }?>
+            
+
+<div class="row" id="row-message">
+    <div class="message-dialog bg-color-yellow fg-color-black" style="display: none;" id="confirm-template" >
+        <p id="message-confirm">Content for message dialog</p>
+        <button class="place-right" id="cancel-confirm-message">Batal</button>
+        <button class="place-right" id="accept-confirm-message" data-id="">Ya</button>
+    </div>
+</div>
+
+<script type="text/javascript">
+
+    $('#quiz-my-quiz-result').click(function(){
+        $('#message').html("Loading Data");
+        $('#loading-template').show();
+
+        $('#content-right').load("<?php echo site_url('quiz/show_my_quiz_result') ?>",function(){
+            $('#loading-template').fadeOut("slow");
+
+        });
+    });
+
+    $('#assignment-add-form').click(function(){
+        $('#message').html("Loading Data");
+        $('#loading-template').show();
+        $('#content-right').load("<?php echo site_url('assignment/show_form_add_assignment') ?>",function(){
+            $('#loading-template').fadeOut("slow");
+        });
+    });
+
+    $('#quiz-manage-resource').click(function(){
+        $('#message').html("Loading Data");
+        $('#loading-template').show();
+        $('#content-right').load("<?php echo site_url('quiz/show_add_resource') ?>",function(){
+            $('#loading-template').fadeOut("slow");
+            
+        });
+    });
+
+    
+    $('#quiz-manage-result').click(function(){
+        $('#message').html("Loading Data");
+        $('#loading-template').show();
+        $('#content-right').load("<?php echo site_url('quiz/show_manage_result') ?>",function(){
+            $('#loading-template').fadeOut("slow");
+
+        });
+
+    });
+
+    $('a#btn-list-course').click(function(){
+        $('#message').html('Loading Informasi');
+        $('#loading-template').show();
+        var id_quiz = $(this).attr('data-id');
+        $('#content-right').load("<?php echo site_url('assignment/list_course') ?>/"+id_quiz,function(){
+            $('#loading-template').fadeOut("slow");
+        });
+    });
+    
+
+    $('a#btn-group').click(function(){
+        $('#message').html('Loading Informasi');
+        $('#loading-template').show();
+        var id_quiz = $(this).attr('data-id');
+        $('#content-right').load("<?php echo site_url('quiz/group_quiz') ?>/"+id_quiz,function(){
+            $('#loading-template').fadeOut("slow");
+        });
+    });
+    
+    $('a#btn-edit').click(function(){
+        $('#message').html('Loading Informasi');
+        $('#loading-template').show();
+        var id_assignment = $(this).attr('data-id');
+        $('#content-right').load("<?php echo site_url('assignment/show_form_edit_assignment') ?>/"+id_assignment,function(){
+            $('#loading-template').fadeOut("slow");
+        });
+    });
+
+    $('a#btn-preview').click(function(){
+        $('#message').html('Loading ...');
+        $('#loading-template').show();
+        var id_quiz = $(this).attr('data-id');
+        $('#content-right').load("<?php echo site_url('quiz/preview_quiz') ?>/"+id_quiz,function(){
+            $('#loading-template').fadeOut("slow");
+        });
+    });
+
+    $('a#btn-config').click(function(){
+        $('#message').html('Loading Informasi');
+        $('#loading-template').show();
+        var id_quiz = $(this).attr('data-id');
+        $('#content-right').load("<?php echo site_url('quiz/list_all_soal') ?>/"+id_quiz,function(){
+            $('#loading-template').fadeOut("slow");
+        });
+    });
+    
+    
+    $('a#btn-delete').click(function(){
+        $('#message-confirm').html('Apakah Anda yakin akan menghapus tugas ini ? ');
+        $('#accept-confirm-message').attr('data-id', $(this).attr('data-id'));
+        $('#confirm-template').fadeIn("medium");
+    });
+    
+
+      $('#accept-confirm-message').click(function(){
+            $('#message').html('Sedang Menghapus .... ');
+            $('#confirm-template').fadeOut("medium");
+            $('#loading-template').fadeIn("slow");
+            var id_quiz = $(this).attr('data-id');
+            $.ajax({
+                type:'POST',
+                url:"<?php echo site_url('assignment/delete_assignment') ?>/"+id_quiz,
+                data:id_quiz,
+                success:function (data) {
+                    $('#content-right').load("<?php echo site_url('assignment/index') ?>",function(){
+                        $('#loading-template').fadeOut("slow");
+                    });
+                },
+                error:function (data){
+                    $('#loading-template').fadeOut("slow");
+                    alert('gagal');
+                }
+            });
+            return false;
+    });
+
+    $('#cancel-confirm-message').click(function(){
+            $('#confirm-template').fadeOut("medium");
+    });
+
+
+    
+    $('table#my-table').each(function() {
+        var currentPage = 0;
+        var numPerPage = 10;
+        var $table = $(this);
+        $table.bind('repaginate', function() {
+            $table.find('tbody tr').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+        });
+        $table.trigger('repaginate');
+        var numRows = $table.find('tbody tr').length;
+        var numPages = Math.ceil(numRows / numPerPage);
+        var $pager = $('<div class="toolbar"></div>');
+        var $pagers = $('<div class="toolbar"></div>');
+        for (var page = 0; page < numPages; page++) {
+            $('<a class="button page-number" style="cursor:pointer;margin-right:4px;"></a>').text(page + 1).bind('click', {
+                newPage: page
+            }, function(event) {
+                currentPage = event.data['newPage'];
+                $table.trigger('repaginate');
+                $(this).addClass('active').siblings().removeClass('active');
+            }).appendTo($pager).addClass('clickable');
+
+            $('<a class="button page-number" style="cursor:pointer;margin-right:4px;"></a>').text(page + 1).bind('click', {
+                newPage: page
+            }, function(event) {
+                currentPage = event.data['newPage'];
+                $table.trigger('repaginate');
+                $(this).addClass('active').siblings().removeClass('active');
+            }).appendTo($pagers).addClass('clickable');
+        }
+        $pager.insertBefore($table).find('span.page-number:first').addClass('active');
+        $pagers.insertAfter($table).find('span.page-number:first').addClass('active');
+
+    });
+
+</script>
