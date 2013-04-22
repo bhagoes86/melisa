bbbc40e760fe80728a614f3c5bfb66deac0a6d65
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-04-22 10:34:30 +0700

    course

--- application/modules/course/views/table_content_viewer.php
+++ application/modules/course/views/table_content_viewer.php
@@ -10,7 +10,14 @@
     .flowplayer .fp-buffer { background-color: rgba(249, 249, 249, 1)}
 </style>
 <h3 style="padding-top: 0px;margin-top: 0px;font-weight: bold;">Materi</h3>
-<table class="striped bordered">
+<div class='pager toolbar' style="vertical-align: middle;">
+    <a style="cursor: pointer;text-decoration: none;" alt='First' class='firstPage button'><i class="icon-first"></i></a>
+    <a style="cursor: pointer;text-decoration: none;" alt='Previous' class='prevPage button'><i class="icon-arrow-left-2"></i></a>
+    <span class='currentPage'></span> dari <span class='totalPages'></span>
+    <a style="cursor: pointer;text-decoration: none;" alt='Next' class='nextPage button'><i class="icon-arrow-right-2"></i></a>
+    <a style="cursor: pointer;text-decoration: none;" alt='Last' class='lastPage button'><i class="icon-last"></i></a>
+</div>
+<table class="striped bordered" id="my-table">
     <?php foreach ($content as $row): ?>
         <tbody>
             <tr style="padding: 0px;margin: 0px;">
@@ -101,3 +108,6 @@
         </tbody>
     <?php endforeach; ?>
 </table>
+<script type="text/javascript">
+    $('#my-table').paginateTable({ rowsPerPage: 1 });
+</script>
