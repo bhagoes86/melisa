ba683fe1a71f4a35e4b8c37ab0d9729a05d037a0
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-04-22 10:41:09 +0700

    course

--- application/modules/course/views/table_content_viewer.php
+++ application/modules/course/views/table_content_viewer.php
@@ -1,6 +1,7 @@
 <script type="text/javascript" src="<?php echo base_url(); ?>asset/metro/js/assets/jquery-1.8.2.min.js"></script>
 <script src="<?php echo base_url() ?>asset/flowplayer/flowplayer.min.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>asset/flowplayer/skin/minimalist.css" />
+<script type="text/javascript" src="<?php echo base_url(); ?>asset/js/jquery.paginatetable.js"></script>
 <style type="text/css">
     /* custom player skin */
     .flowplayer { background-color: #222; background-size: cover; }
