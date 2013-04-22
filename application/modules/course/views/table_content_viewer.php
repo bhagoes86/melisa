182afff8b96a69d8e0ea82ab542d478232e25c5e
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-04-22 10:17:25 +0700

    course

--- application/modules/course/views/table_content_viewer.php
+++ application/modules/course/views/table_content_viewer.php
@@ -36,7 +36,33 @@
                     <?php } elseif ($row->type == 1) { ?>
 
                     <?php } elseif ($row->type == 2) { ?>
-
+                        <div style="background-color: #000;height: 394px;">
+                            <?php
+                            $media = analyze_media($row->file);
+                            $trace = explode('^^^', $media);
+                            switch ($trace[0]) {
+                                case 'image' :
+                                    echo "<a href='" . $trace[3] . "' target='_blank'><img src='" . $trace[3] . "' width='100%' /></a>";
+                                    break;
+                                case 'youtube' :
+                                    echo youtube($trace[1]);
+                                    break;
+                                case 'vimeo' :
+                                    echo vimeo($trace[1]);
+                                    break;
+                                case 'scribd' :
+                                    echo scribd($trace[1]);
+                                    break;
+                                case 'docstoc' :
+                                    echo docstoc($trace[1]);
+                                    break;
+                                case 'link' :
+                                    break;
+                                default :
+                                    die;
+                            }
+                            ?>
+                        </div>
                     <?php } ?>
                 </td>
             </tr>
