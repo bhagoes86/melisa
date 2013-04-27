60401e7c021602a4937addfdc1f9d7fa13ebee1f
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-04-27 13:47:06 +0700

    wall feed

--- application/modules/content/views/wall_podcast.php
+++ application/modules/content/views/wall_podcast.php
@@ -73,7 +73,7 @@
                             <?php echo $row->title; ?>
                         </a>
                     </h4>
-                    <?php echo cut_text($row->description, 300); ?>
+                    <?php echo CutText($row->description, 300); ?>
                 </div>
                 <div class="clearfix"></div>
             </div>            
