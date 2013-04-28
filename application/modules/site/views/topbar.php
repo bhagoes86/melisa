cc9116fb4af371ce16256fbf42ae4e9c5c49388a
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-04-28 16:59:35 +0700

    wallpost

--- application/modules/site/views/topbar.php
+++ application/modules/site/views/topbar.php
@@ -25,7 +25,12 @@
                     <?php echo modules::run('course/menu_faculty') ?>
                 </ul>
             </li>
-            <li><a href="javascript:void(0)" id="btn-new-vid"><?php echo $topbar->menu3; ?></a></li>                
+            <li>
+                <a href="javascript:void(0)" id="btn-new-vid"><?php echo $topbar->menu3; ?></a>
+                <ul class="dropdown-menu" id="menu_bimbel">
+                    <?php echo modules::run('course/menu_bimbel') ?>
+                </ul>
+            </li>   
         </ul>
     </div>
 </div>
