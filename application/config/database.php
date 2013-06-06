b22979deffb38e95997783fe2f8d993318cc8caf
root <taufiksu@gmail.com>
2013-06-06 12:46:14 +0700

    db

--- application/config/database.php
+++ application/config/database.php
@@ -46,11 +46,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-<<<<<<< HEAD
 $db['default']['password'] = 'keripiksingkong';
-=======
-$db['default']['password'] = '';
->>>>>>> 11f64480b5934c02988e01cfbac23dd09320ba74
 $db['default']['database'] = 'sakola';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
