791de2f9666f82c559b5d52df7d949b5a4884450
Taufik Sulaeman P <taufiksu@gmail.com>
2013-06-27 20:26:57 +0700

    download button

--- application/modules/forum/views/btn_download.php
+++ application/modules/forum/views/btn_download.php
@@ -1,8 +1,8 @@
 <button title="Unduh" id="downloads<?php echo $file ?>" data-file="<?php echo $file ?>"><i class="icon-download"></i></button>
 <script type="text/javascript">
-    $('#downloads<?php echo $file ?>').click(function(){
+    $('button#downloads<?php echo $file ?>').click(function(){
         file = $(this).attr('data-file');
-        location.href("<?php echo base_url() ?>resource/"+file,"_blank");
+        location.href="<?php echo base_url() ?>resource/"+file,"_blank";
         return false;
     });
 </script>
