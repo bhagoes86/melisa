ab3a890802da72f12f8345b34e848aaf88a43a30
Taufik Sulaeman P <taufiksu@gmail.com>
2013-06-27 13:21:14 +0700

    forum

--- application/modules/forum/views/wall_content_log.php
+++ application/modules/forum/views/wall_content_log.php
@@ -111,7 +111,7 @@
                         <img src="http://img.youtube.com/vi/<?php echo $extract_id[1]; ?>/1.jpg" style="width: 180px;height: 123px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><?php echo word_wrap(nl2br(auto_link($row->file)), 40); ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
@@ -149,7 +149,7 @@
                         <img src="<?php echo ($media['thumbnail_medium']) ?>" style="width: 180px;height: 123px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i> <?php echo word_wrap(nl2br(auto_link($row->file)), 40) ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
@@ -261,7 +261,7 @@
                         <img src="<?php echo "http:" . $thumbnail ?>" style="width: 180px;height: 123px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i> <?php echo word_wrap(nl2br(auto_link($row->file)), 40) ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
@@ -304,7 +304,7 @@
                         <img src="http://img.docstoccdn.com/thumb/100/<?php echo $extract_id[1] ?>.png" style="width: 120px;height: 135px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i> <?php echo splitPhrase($row->file, 15) ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
@@ -338,65 +338,6 @@
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
                 <!--more todo-->
-            </div>
-        </li>
-    <?php } elseif ($row->type == 9) { ?>
-        <!--picture-->
-        <li class="feed-link" id="content<?php echo $row->id_content ?>">
-            <span class="feed-avatar">
-                <?php if ($row->profic == '') { ?>
-                    <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                <?php } else { ?>
-                    <?php
-                    $profpic = 'resource/' . $row->profic;
-                    if (file_exists($profpic)) {
-                        ?>
-                        <img src="<?php echo base_url() . $profpic ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } else { ?>
-                        <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } ?>
-                <?php } ?>
-            </span>
-            <div class="data">
-                <div class="user-description">
-                    <h4>
-                        <a href="<?php echo site_url('forum' . '/' . $row->user_id) ?>"><?php echo modules::run('authz/get_username', $row->user_id) ?></a>                     
-                    </h4>
-                    <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
-                </div>
-                <div class="image video-image" style="margin-bottom: 0px;padding-bottom: 0px;">
-                    <img src="<?php echo base_url() . 'resource/' . $row->file ?>" style="margin-bottom: 0px;padding-bottom: 0px;">
-                    <div class="description"><?php echo nl2br($row->description) ?></div>
-                </div>
-            </div>
-        </li>
-    <?php } elseif ($row->type == 10) { ?>
-        <!--plain-->
-        <li class="feed-link" id="content<?php echo $row->id_content ?>">
-            <span class="feed-avatar">
-                <?php if ($row->profic == '') { ?>
-                    <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                <?php } else { ?>
-                    <?php
-                    $profpic = 'resource/' . $row->profic;
-                    if (file_exists($profpic)) {
-                        ?>
-                        <img src="<?php echo base_url() . $profpic ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } else { ?>
-                        <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } ?>
-                <?php } ?>
-            </span>
-            <div class="data">
-                <div class="user-description">
-                    <h4>
-                        <a href="<?php echo site_url('forum' . '/' . $row->user_id) ?>"><?php echo modules::run('authz/get_username', $row->user_id) ?></a> 
-                    </h4>
-                    <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
-                </div>
-                <div class="text" style="border-top: 1px solid #bbb;">
-                    <p><?php echo nl2br($row->description) ?></p>
-                </div>
             </div>
         </li>
     <?php } ?>
