60074a605b536d4d00f5eb1ee3ee0f5c964f1380
Taufik Sulaeman P <taufiksu@gmail.com>
2013-06-28 10:43:12 +0700

    document wall

--- application/modules/forum/views/wall_content_log.php
+++ application/modules/forum/views/wall_content_log.php
@@ -6,6 +6,7 @@
 
     .pager li{
         float:left;
+        margin-left: 7px;
         list-style-type:none;
         margin-right:.3em;
         font-size:1.1em;
@@ -46,36 +47,44 @@
                     </div>
                     <div class="clearfix"></div>
                 </div>                
-                <div class="text"><?php echo $row->description ?></div>  
+                <div class="text"><?php echo nl2br($row->description) ?></div>
+                <div class="utils">                    
+                    <div class="toolbar place-left"></div>
+                    <div class="toolbar place-right">
+                        <?php echo modules::run('forum/btn_tags', $row->id_content, 0) ?>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <?php echo modules::run('forum/form_tag_add', $row->id_content, 0) ?>                        
             </div>
         </li>
     <?php } elseif ($row->type == 1) { ?>
-        <!--document-->
-        <li class="feed-link" id="content<?php echo $row->id_content ?>">
-            <span class="feed-avatar">
-                <?php if ($row->profic == '') { ?>
-                    <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                <?php } else { ?>
-                    <?php
-                    $profpic = 'resource/' . $row->profic;
-                    if (file_exists($profpic)) {
-                        ?>
-                        <img src="<?php echo base_url() . $profpic ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } else { ?>
-                        <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } ?>
-                <?php } ?>
-            </span>
-            <div class="data">
-                <div class="user-description">
-                    <h4>
-                        <a href="<?php echo site_url('forum' . '/' . $row->user_id) ?>"><?php echo modules::run('authz/get_username', $row->user_id) ?></a> 
-                        <a href="javascript:void(0)" target="_blank" style="text-decoration: none;cursor: pointer;color: #2f3e46;">mengunggah <i class="icon-libreoffice"></i>&nbsp;&nbsp;konten dokumen</a>
-                    </h4>
-                    <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
-                </div>
-                <!--more todo-->
+        class="user-description">
+        <h4>
+            <a href="<?php echo site_url('forum' . '/' . $row->user_id) ?>"><?php echo modules::run('authz/get_username', $row->user_id) ?></a> 
+        </h4>
+        <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
+        </div>
+        <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
+        <div class="image link-image">
+            <a href="javascript:void(0)" id="pic-content-activate" data-id="<?php echo $row->id_content ?>">
+                <img src="<?php echo base_url() . 'resource/' . $row->id_content . '.jpg' ?>" style="width: 120px;height: 135px;vertical-align: middle;border-right: 1px solid #bbb;">
+            </a>
+            <div class="description">
+                <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
             </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="text"><p><?php echo nl2br($row->description) ?></p></div>
+        <div class="utils">                    
+            <div class="toolbar place-left"></div>
+            <div class="toolbar place-right">
+                <?php echo modules::run('forum/btn_tags', $row->id_content, 1) ?>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <?php echo modules::run('forum/form_tag_add', $row->id_content, 1) ?> 
+        </div>
         </li>
     <?php } elseif ($row->type == 2) { ?>
         <!--Youtube-->
@@ -115,7 +124,15 @@
                     </div>
                     <div class="clearfix"></div>
                 </div>
-                <div class="text"><?php echo $row->description ?></div>                
+                <div class="text"><?php echo nl2br($row->description) ?></div>
+                <div class="utils">                    
+                    <div class="toolbar place-left"></div>
+                    <div class="toolbar place-right">
+                        <?php echo modules::run('forum/btn_tags', $row->id_content, 2) ?>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <?php echo modules::run('forum/form_tag_add', $row->id_content, 2) ?>                        
             </div>
         </li>
     <?php } elseif ($row->type == 3) { ?>
@@ -153,10 +170,18 @@
                     </div>
                     <div class="clearfix"></div>
                 </div>
-                <div class="text"><?php echo $row->description ?></div>                
+                <div class="text"><?php echo nl2br($row->description) ?></div> 
+                <div class="utils">                    
+                    <div class="toolbar place-left"></div>
+                    <div class="toolbar place-right">
+                        <?php echo modules::run('forum/btn_tags', $row->id_content, 3) ?>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <?php echo modules::run('forum/form_tag_add', $row->id_content, 3) ?>  
             </div>
         </li>
-    <?php } elseif ($row->type == 4) { ?>
+    <?php } elseif ($row->type == 5) { ?>
         <!--scribd-->
         <li class="feed-link" id="content<?php echo $row->id_content ?>">
             <span class="feed-avatar">
@@ -184,44 +209,15 @@
                 <div class="text" style="border: 1px solid #bbb;color: #bbb;">
                     <a id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-file-pdf"></i>&nbsp;<?php echo $row->file ?></a>
                 </div>
-                <div class="text">
-                    <p><?php echo nl2br($row->description) ?></p>
+                <div class="text"><p><?php echo nl2br($row->description) ?></p></div>
+                <div class="utils">                    
+                    <div class="toolbar place-left"></div>
+                    <div class="toolbar place-right">
+                        <?php echo modules::run('forum/btn_tags', $row->id_content, 4) ?>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
-            </div>
-        </li>
-    <?php } elseif ($row->type == 6) { ?>
-        <!--SoundCloud-->
-        <li class="feed-link" id="content<?php echo $row->id_content ?>">
-            <span class="feed-avatar">
-                <?php if ($row->profic == '') { ?>
-                    <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                <?php } else { ?>
-                    <?php
-                    $profpic = 'resource/' . $row->profic;
-                    if (file_exists($profpic)) {
-                        ?>
-                        <img src="<?php echo base_url() . $profpic ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } else { ?>
-                        <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } ?>
-                <?php } ?>
-            </span>
-            <div class="data">
-                <div class="user-description">
-                    <h4>
-                        <a href="<?php echo site_url('forum' . '/' . $row->user_id) ?>"><?php echo modules::run('authz/get_username', $row->user_id) ?></a> 
-                    </h4>
-                    <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
-                </div>
-                <div class="text" style="background: #bbb;border:1px solid #bbb;vertical-align: middle;padding: 0px;width: 100%;height: 168px;">
-                    <div id="putTheWidgetHere-<?php echo $row->file ?>" style="height: 168px;"></div>
-                    <script type="text/JavaScript">
-                        SC.oEmbed("<?php echo $row->file ?>", {color: "ff0066"},  document.getElementById("putTheWidgetHere-<?php echo $row->file ?>"));
-                    </script>
-                </div>
-                <div class="text">
-                    <p><?php echo nl2br($row->description) ?></p>
-                </div>
+                <?php echo modules::run('forum/form_tag_add', $row->id_content, 4) ?> 
             </div>
         </li>
     <?php } elseif ($row->type == 5) { ?>
@@ -261,13 +257,60 @@
                         <img src="<?php echo "http:" . $thumbnail ?>" style="width: 180px;height: 123px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
-                <div class="text">
-                    <p><?php echo nl2br($row->description) ?></p>
+                <div class="text"><p><?php echo nl2br($row->description) ?></p></div>
+                <div class="utils">                    
+                    <div class="toolbar place-left"></div>
+                    <div class="toolbar place-right">
+                        <?php echo modules::run('forum/btn_tags', $row->id_content, 5) ?>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
+                <?php echo modules::run('forum/form_tag_add', $row->id_content, 5) ?>  
+            </div>
+        </li>
+    <?php } elseif ($row->type == 6) { ?>
+        <!--SoundCloud-->
+        <li class="feed-link" id="content<?php echo $row->id_content ?>">
+            <span class="feed-avatar">
+                <?php if ($row->profic == '') { ?>
+                    <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
+                <?php } else { ?>
+                    <?php
+                    $profpic = 'resource/' . $row->profic;
+                    if (file_exists($profpic)) {
+                        ?>
+                        <img src="<?php echo base_url() . $profpic ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
+                    <?php } else { ?>
+                        <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
+                    <?php } ?>
+                <?php } ?>
+            </span>
+            <div class="data">
+                <div class="user-description">
+                    <h4>
+                        <a href="<?php echo site_url('forum' . '/' . $row->user_id) ?>"><?php echo modules::run('authz/get_username', $row->user_id) ?></a> 
+                    </h4>
+                    <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
+                </div>
+                <div class="text" style="background: #bbb;border:1px solid #bbb;vertical-align: middle;padding: 0px;width: 100%;height: 168px;">
+                    <div id="putTheWidgetHere-<?php echo $row->file ?>" style="height: 168px;"></div>
+                    <script type="text/JavaScript">
+                        SC.oEmbed("<?php echo $row->file ?>", {color: "ff0066"},  document.getElementById("putTheWidgetHere-<?php echo $row->file ?>"));
+                    </script>
+                </div>
+                <div class="text"><p><?php echo nl2br($row->description) ?></p></div>
+                <div class="utils">                    
+                    <div class="toolbar place-left"></div>
+                    <div class="toolbar place-right">
+                        <?php echo modules::run('forum/btn_tags', $row->id_content, 6) ?>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <?php echo modules::run('forum/form_tag_add', $row->id_content, 6) ?>  
             </div>
         </li>
     <?php } elseif ($row->type == 7) { ?>
@@ -304,15 +347,21 @@
                         <img src="http://img.docstoccdn.com/thumb/100/<?php echo $extract_id[1] ?>.png" style="width: 120px;height: 135px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-link"></i><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><?php echo word_wrap(nl2br(auto_link($row->title)), 40); ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
-                <div class="text">
-                    <p><?php echo nl2br($row->description) ?></p>
+                <div class="text"><p><?php echo nl2br($row->description) ?></p></div>
+                <div class="utils">                    
+                    <div class="toolbar place-left"></div>
+                    <div class="toolbar place-right">
+                        <?php echo modules::run('forum/btn_tags', $row->id_content, 7) ?>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
+                <?php echo modules::run('forum/form_tag_add', $row->id_content, 7) ?> 
             </div>
-        </li>   
+        </li>  
     <?php } ?>
 <?php endforeach; ?>
 <script src="http://connect.soundcloud.com/sdk.js"></script>
@@ -320,35 +369,18 @@
     $('#wall_container').flexipage({
         perpage:10
     });
-    //delete
-    $("button#remove-status").click(function(){
-        var id = $(this).attr('data-id');
-        var answer = confirm('Apakah anda yakin akan menghapus konten ini ?')
-        if (answer == true){            
-            $('#wall'+id).fadeOut('slow');        
-            $.ajax({
-                url: "<?php echo site_url('forum/delete_wall') ?>/"+id,
-                success: function(){
-                    $('#wall'+id).fadeOut('slow');
-                }
-            });
-        }else{
-            alert('Konten tidak jadi dihapus');
-        }        
-        return false;
-    });
     //text activate preview
     $('a#btn-content-activate').click(function(){
         var id_content = $(this).attr('data-id');
         $('#wall-content-viewer-'+id_content).slideToggle('fast');
-        $('#wall-content-viewer-'+id_content).load("<?php echo site_url('forum/wall_player') ?>/"+id_content);           
+        $('#wall-content-viewer-'+id_content).load("<?php echo site_url('forum/wall_content_player') ?>/"+id_content);           
         return false;
     });
     //picture activate preview
     $('a#pic-content-activate').click(function(){
         var id_content = $(this).attr('data-id');
         $('#wall-content-viewer-'+id_content).slideToggle('fast');
-        $('#wall-content-viewer-'+id_content).load("<?php echo site_url('forum/wall_player') ?>/"+id_content);
+        $('#wall-content-viewer-'+id_content).load("<?php echo site_url('forum/wall_content_player') ?>/"+id_content);
         return false;
     });
     //plugin soundcloud
