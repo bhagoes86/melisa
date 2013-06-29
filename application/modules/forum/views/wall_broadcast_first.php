67cdce4ceb22a3a0b250d88f0ef9997390195bf9
Taufik Sulaeman P <taufiksu@gmail.com>
2013-06-29 12:16:42 +0700

    wall activity

--- application/modules/forum/views/wall_broadcast_first.php
+++ application/modules/forum/views/wall_broadcast_first.php
@@ -134,7 +134,7 @@
                         <img src="<?php echo ($media['thumbnail_medium']) ?>" style="width: 180px;height: 123px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_wall ?>"><?php echo word_wrap(nl2br(auto_link($row->url)), 40) ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_wall ?>"><?php echo word_wrap(nl2br(auto_link($row->url)), 20) ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
@@ -270,7 +270,7 @@
                         <img src="<?php echo "http:" . $thumbnail ?>" style="width: 180px;height: 123px;vertical-align: middle;border-right: 1px solid #bbb;">
                     </a>
                     <div class="description">
-                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_wall ?>"><?php echo word_wrap(nl2br(auto_link($row->url)), 40) ?></a>
+                        <a href="javascript:void(0)" id="btn-content-activate" data-id="<?php echo $row->id_wall ?>"><?php echo word_wrap(nl2br(auto_link($row->url)), 20) ?></a>
                     </div>
                     <div class="clearfix"></div>
                 </div>
@@ -336,33 +336,6 @@
                     </div>
                     <div class="clearfix"></div>
                 </div>
-            </div>
-        </li>
-    <?php } elseif ($row->forum_type == 8) { ?>
-        <!--proprofs-->
-        <li class="feed-link" id="wall<?php echo $row->id_wall ?>">
-            <span class="feed-avatar">
-                <?php if ($row->profic == '') { ?>
-                    <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                <?php } else { ?>
-                    <?php
-                    $profpic = 'resource/' . $row->profic;
-                    if (file_exists($profpic)) {
-                        ?>
-                        <img src="<?php echo base_url() . $profpic ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } else { ?>
-                        <img src="<?php echo base_url() . 'asset/css/images/photo-default.png' ?>" class="userphoto" style="padding-right: 0px;width: 100%;height: 59px;"/>
-                    <?php } ?>
-                <?php } ?>
-            </span>
-            <div class="data">
-                <div class="user-description">
-                    <h4>
-                        <a href="<?php echo site_url('forum' . '/' . $row->user_id) ?>"><?php echo modules::run('authz/get_username', $row->user_id) ?></a>                     
-                    </h4>
-                    <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
-                </div>
-                <!--more todo-->
             </div>
         </li>
     <?php } elseif ($row->forum_type == 9) { ?>
