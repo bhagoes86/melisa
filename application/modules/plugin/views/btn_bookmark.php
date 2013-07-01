47a0cbc67072a5a14c90d6dd7e82dd1a03b95cc0
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-01 22:13:03 +0700

    delete bookmark

--- application/modules/plugin/views/btn_bookmark.php
+++ application/modules/plugin/views/btn_bookmark.php
@@ -1,13 +1,13 @@
 <?php if ($bookmark_status == '') { ?>
-    <button title="bookmark" id="bookmark-<?php echo $content_id ?>" data-content="<?php echo $content_id ?>" data-type="<?php echo $type ?>"><i class="icon-bookmark"></i>&nbsp;</button>
+    <button title="bookmark" id="bookmark<?php echo $content_id ?>" data-content="<?php echo $content_id ?>" data-type="<?php echo $type ?>"><i class="icon-bookmark"></i>&nbsp;</button>
     <script type="text/javascript">
-        $("button#bookmark-<?php echo $content_id ?>").click(function(){
+        $("button#bookmark<?php echo $content_id ?>").click(function(){
             var content_id = $(this).attr('data-id');
             var type = $(this).attr('data-type');
             $.ajax({
                 url: "<?php echo site_url('plugin/add_bookmark_me') ?>/"+content_id+'/'+type,
                 success: function(){
-                    $('button#bookmark-'+content_id).fadeOut('fast');
+                    $('button#bookmark'+content_id).fadeOut('fast');
                 }
             });
             return false;
