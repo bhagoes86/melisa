e00cf646c30de69570471cfa2b088d877b133af5
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-01 21:30:27 +0700

    delete bookmark

--- application/modules/plugin/views/btn_bookmark_remove.php
+++ application/modules/plugin/views/btn_bookmark_remove.php
@@ -1,6 +1,6 @@
-<button title="Hapus" id="remove-bookmark" data-id="<?php echo $content_id ?>" data-type="<?php echo $type ?>" style="text-align: center;"><i class="icon-cancel"></i>&nbsp;</button>
+<button title="Hapus" id="remove-bookmark<?php echo $content_id ?>" data-id="<?php echo $content_id ?>" data-type="<?php echo $type ?>" style="text-align: center;"><i class="icon-cancel"></i>&nbsp;</button>
 <script type="text/javascript">
-    $("button#remove-bookmark").click(function(){
+    $("button#remove-bookmark<?php echo $content_id ?>").click(function(){
         var content_id = $(this).attr('data-id');
         var type = $(this).attr('data-type');
         var answer = confirm('Apakah anda yakin akan menghapus konten ini ?')
