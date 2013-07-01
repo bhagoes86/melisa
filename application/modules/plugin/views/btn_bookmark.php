bb60ce09fd2e35cf354c813c9fa7907f1f67e30f
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-01 22:14:58 +0700

    add bookmark

--- application/modules/plugin/views/btn_bookmark.php
+++ application/modules/plugin/views/btn_bookmark.php
@@ -2,7 +2,7 @@
     <button title="bookmark" id="bookmark<?php echo $content_id ?>" data-content="<?php echo $content_id ?>" data-type="<?php echo $type ?>"><i class="icon-bookmark"></i>&nbsp;</button>
     <script type="text/javascript">
         $("button#bookmark<?php echo $content_id ?>").click(function(){
-            var content_id = $(this).attr('data-id');
+            var content_id = $(this).attr('data-content');
             var type = $(this).attr('data-type');
             $.ajax({
                 url: "<?php echo site_url('plugin/add_bookmark_me') ?>/"+content_id+'/'+type,
