e8b706df8f8b457296d1f7f120bc39a95c56710f
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-01 09:29:44 +0700

    tags integration

--- application/modules/plugin/views/wall_content_tag_list.php
+++ application/modules/plugin/views/wall_content_tag_list.php
@@ -1,5 +1,5 @@
 <div class="text" style="border-top: 1px solid #bbb;">
     <?php foreach ($content as $row): ?>
-        <a href="javascript:void(0)"><?php echo $row->tags ?></a>
+        <a href="javascript:void(0)"><?php echo $row->tag ?></a>
     <?php endforeach; ?>
 </div>
