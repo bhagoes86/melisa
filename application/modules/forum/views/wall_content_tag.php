56f3da9428b4487f32a28bbedf286f992c02f2d8
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-01 16:08:33 +0700

    tags integration

--- application/modules/forum/views/wall_content_tag.php
+++ application/modules/forum/views/wall_content_tag.php
@@ -37,7 +37,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
                 <div class="image link-image">
                     <a href="javascript:void(0)" id="pic-content-activate" data-id="<?php echo $row->id_content ?>">
@@ -85,7 +85,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
                 <div class="image link-image">
                     <a href="javascript:void(0)" id="pic-content-activate" data-id="<?php echo $row->id_content ?>">
@@ -134,7 +134,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
                 <div class="image link-image">
                     <?php
@@ -187,7 +187,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
                 <div class="image link-image">
                     <?php $media = vimeo_cover($row->file); ?>
@@ -237,7 +237,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
                 <div class="text" style="border: 1px solid #bbb;color: #bbb;">
                     <a id="btn-content-activate" data-id="<?php echo $row->id_content ?>"><i class="icon-file-pdf"></i>&nbsp;<?php echo $row->file ?></a>
@@ -280,7 +280,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="height: 380px;display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
                 <div class="image link-image">
                     <?php
@@ -336,7 +336,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" style="background: #bbb;border:1px solid #bbb;vertical-align: middle;padding: 0px;width: 100%;height: 168px;">
                     <div id="putTheWidgetHere-<?php echo $row->file ?>" style="height: 168px;"></div>
                     <script type="text/JavaScript">
@@ -381,7 +381,7 @@
                     </h4>
                     <span class="date-meta"><?php echo nicetime(dtm2timestamp($row->date)) ?></span>
                 </div>
-                <?php echo modules::run('plugin/tag_content_by_id_limit', $row->id_content, 3) ?>
+                <?php echo modules::run('plugin/tag_content_by_id_limit_in_bookmark', $row->id_content, 3) ?>
                 <div class="text" id="wall-content-viewer-<?php echo $row->id_content ?>" data-id="<?php echo $row->id_content ?>" style="display: none;padding: 0px;vertical-align: middle;margin: 0px;background: rgba(0,0,0,0.10);"></div>            
                 <div class="image link-image">
                     <?php
--- application/modules/plugin/controllers/plugin.php
+++ application/modules/plugin/controllers/plugin.php
@@ -34,10 +34,22 @@ class Plugin extends MX_Controller {
         $data['tag_counter'] = count($data['content']);
         $this->load->view('plugin/wall_content_tag_list', $data);
     }
+    
+    function tag_content_by_id_limit_in_bookmark($content_id, $limit) {
+        $data['content_id'] = $content_id;
+        $data['content'] = $this->model_plugin->select_tags_content($content_id, $limit)->result();
+        $data['tag_counter'] = count($data['content']);
+        $this->load->view('plugin/wall_content_tag_list_in_bookmark', $data);
+    }
 
     function get_content_from_tag($tag) {
         $data['content'] = $this->model_plugin->select_tag_join_content(urldecode($tag))->result();
         $this->load->view('plugin/wall_content_layout', $data);
+    }
+    
+    function get_content_from_tag_in_bookmark($tag) {
+        $data['content'] = $this->model_plugin->select_tag_join_content(urldecode($tag))->result();
+        $this->load->view('plugin/wall_content_bookmark_in_bookmark', $data);
     }
 
 }
