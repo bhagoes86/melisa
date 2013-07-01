2d243884ce4f94906e5499c43511d2fe2925e6e7
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-01 09:31:35 +0700

    tags integration

--- application/modules/plugin/controllers/plugin.php
+++ application/modules/plugin/controllers/plugin.php
@@ -29,7 +29,7 @@ class Plugin extends MX_Controller {
     }
 
     function tag_content_by_id_limit($content_id, $limit) {
-        $data['tags'] = $this->model_plugin->select_tags_content($content_id, $limit)->result();
+        $data['content'] = $this->model_plugin->select_tags_content($content_id, $limit)->result();
         $this->load->view('plugin/wall_content_tag_list', $data);
     }
 
