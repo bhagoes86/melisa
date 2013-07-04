ce25af99697497d4f05ecef6138d201b2fb4c0f3
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-04 15:47:13 +0700

    widget podcast time

--- application/modules/forum/views/widget_document_meta_faculty.php
+++ application/modules/forum/views/widget_document_meta_faculty.php
@@ -1,4 +1,4 @@
-<fieldset class="feed" style="margin-top: 5px;overflow-x: ">
+<!--<fieldset class="feed" style="margin-top: 5px;overflow-x: ">
     <legend><i class="icon-filter"></i>Filter Fakultas</legend>
     <ul style="list-style:none;color:#aaa">
         <li>
@@ -25,5 +25,33 @@
             }, 1000);
         });
         return false; 
+    });
+</script>-->
+
+<fieldset class="feed" style="margin-top: 5px;">
+    <legend style="padding-top: 2px;"><i class="icon-filter"></i>Filter Dokumen Fakultas</legend>
+    <div class="input-control select" style="width: 100%">
+        <select id="document-filter-faculty" style="width: 100%">
+            <option href="javascript:void(0)">Pilih Fakultas</option>
+            <?php foreach ($content as $row): ?>
+                <option href="javascript:void(0)" value="<?php echo $row->id_faculty ?>"><?php echo $row->faculty ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+</fieldset>
+<script type="text/javascript">
+    $("select#document-filter-faculty").change(function(){
+        var id_faculty = $("#document-filter-faculty").val();
+        $('div.pager').remove(); 
+        $('#message').html("Loading Data");
+        $('#loading-template').show(); 
+        $('#wall_container').empty(); 
+        $('#wall_container').load("<?php echo site_url('forum/wall_content_document_faculty') ?>/"+id_faculty,function(){            
+            $('#loading-template').fadeOut('slow');
+            $("html, body").animate({
+                scrollTop: 0
+            }, 1000);
+        });
+        return false;
     });
 </script>
