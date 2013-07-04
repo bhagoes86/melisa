7fb96a1abacbf7c56d793ff284c403007087fa5e
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-04 12:12:28 +0700

    widget podcast time

--- application/modules/forum/views/widget_podcast_meta_faculty.php
+++ application/modules/forum/views/widget_podcast_meta_faculty.php
@@ -2,12 +2,12 @@
     <legend><i class="icon-filter"></i>Filter Fakultas</legend>
     <ul style="list-style:none;color:#aaa">
         <li>
-            <?php foreach ($content as $row): ?>
-                <label class="input-control radio fg-color-greenDark">
-                    <input checked="" type="radio" name="id_faculty" id="id_faculty" data-id="<?php echo $row->id_faculty ?>">
-                    <span class="helper" title="<?php echo $row->faculty ?>"><?php echo $row->short ?></span> 
-                </label><br/>
-            <?php endforeach; ?>
+<?php foreach ($content as $row): ?>
+                    <label class="input-control radio fg-color-greenDark">
+                        <input checked="" type="radio" name="id_faculty" id="id_faculty" data-id="<?php echo $row->id_faculty ?>">
+                        <span class="helper" title="<?php echo $row->faculty ?>"><?php echo $row->short ?></span> 
+                    </label><br/>
+<?php endforeach; ?>
         </li>
     </ul>
 </fieldset>
@@ -31,8 +31,8 @@
 <fieldset class="feed" style="margin-top: 5px;">
     <legend><i class="icon-filter"></i>Filter Fakultas</legend>
     <div class="input-control select" style="width: 100%">
-        <select class="fg-color-orangeDark" style="width: 100%">
-                <option class="fg-color-orangeDark" href="javascript:void(0)" id="podcast_option_faculty" value="<?php echo $row->id_faculty ?>">Pilih Fakultas</option>
+        <select id="podcast-filter-faculty" class="fg-color-orangeDark" style="width: 100%">
+            <option class="fg-color-orangeDark" href="javascript:void(0)" id="podcast_option_faculty" value="<?php echo $row->id_faculty ?>">Pilih Fakultas</option>
             <?php foreach ($content as $row): ?>
                 <option class="fg-color-orangeDark" href="javascript:void(0)" id="podcast_option_faculty" value="<?php echo $row->id_faculty ?>"><?php echo $row->faculty ?></option>
             <?php endforeach; ?>
@@ -40,7 +40,7 @@
     </div>
 </fieldset>
 <script type="text/javascript">
-    $("select").change(function(){
+    $("select#podcast-filter-faculty").change(function(){
         var id_faculty = $("#podcast_option_faculty").val();
         $('div.pager').remove(); 
         $('#message').html("Loading Data");
