5a1471de6b3e6ae7df4a4ddf893ada8ce4f3d9b5
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-04 11:48:56 +0700

    widget podcast time

--- application/modules/forum/views/widget_podcast_meta_time.php
+++ application/modules/forum/views/widget_podcast_meta_time.php
@@ -2,7 +2,7 @@
     <legend><i class="icon-filter"></i>Filter Waktu</legend>
     <div class="input-control select">
         <select id="podcast_option_time">
-            <option href="javascript:void(0)" id="podcast_option" value="<?php echo date('Y'); ?>"><a href="javascript:void(0)" id="podcast_time" data-year="<?php echo date('Y'); ?>">Pilih </a></option>
+            <option href="javascript:void(0)" id="podcast_option" value="<?php echo date('Y'); ?>"><a href="javascript:void(0)" id="podcast_time" data-year="<?php echo date('Y'); ?>">-- Pilih Waktu --</a></option>
             <option href="javascript:void(0)" id="podcast_option" value="<?php echo date('Y'); ?>"><a href="javascript:void(0)" id="podcast_time" data-year="<?php echo date('Y'); ?>">Tahun Sekarang</a></option>
             <option href="javascript:void(0)" id="podcast_option" value="<?php echo date('Y') - 1; ?>"><a href="javascript:void(0)" id="podcast_time" data-year="<?php echo date('Y') - 1; ?>">Tahun Kemarin</a></option>
         </select>
