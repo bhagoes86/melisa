58e259fde6f10504569fa9dc7e2670de7ab74d4d
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-04 11:02:59 +0700

    widget podcast time

--- application/modules/forum/views/widget_podcast_meta_time.php
+++ application/modules/forum/views/widget_podcast_meta_time.php
@@ -1,45 +1,23 @@
 <fieldset class="feed" style="margin-top: 5px;">
     <legend><i class="icon-filter"></i>Filter Waktu</legend>
-    <ul style="list-style:none;color:#aaa">
-        <li>
-            <label class="input-control radio fg-color-orangeDark">
-                <input type="radio" name="podcast-year">
-                <span class="helper">Podcast Bulan Sekarang</span>
-            </label><br/>
-            <label class="input-control radio fg-color-orangeDark">
-                <input type="radio" name="podcast-year">
-                <span class="helper">Podcast Bulan Kemarin</span>
-            </label><br/>
-            <label class="input-control radio fg-color-orangeDark">
-                <input type="radio" name="podcast-year" id="podcast-year" data-year="<?php echo date('Y'); ?>">
-                <span class="helper">Podcast Tahun Sekarang</span>
-            </label><br/>
-            <?php $last_year = date('Y') - 1; ?>
-            <label class="input-control radio fg-color-orangeDark">
-                <input type="radio" name="podcast-year" id="podcast-year" date-year="<?php echo $last_year; ?>">
-                <span class="helper">Podcast Tahun Lalu</span>
-            </label><br/>
-            <label class="input-control radio fg-color-orangeDark">
-                <input type="radio" name="podcast-year">
-                <span class="helper">Podcast Tahun < <?php echo date('Y'); ?></span>
-            </label>
-        </li>
+    <ul class="dropdown-menu">
+        <li><a href="javascript:void(0)" id="podcast_time" data-year="<?php echo date('Y'); ?>">Tahun Sekarang</a></li>
+        <li><a href="javascript:void(0)" id="podcast_time" data-year="<?php echo date('Y') - 1; ?>">Tahun Kemarin</a></li>
     </ul>
 </fieldset>
 <script type="text/javascript">
-    $('input[name=podcast-year]:radio').change(function(){
+    $('a#podcast_time').click(function(){
         var year = $(this).attr('data-year');
-        alert(year);
-        //        $('div.pager').remove(); 
-        //        $('#message').html("Loading Data");
-        //        $('#loading-template').show(); 
-        //        $('#wall_container').empty(); 
-        //        $('#wall_container').load("<?php echo site_url('forum/wall_content_podcast_year') ?>/"+year,function(){            
-        //            $('#loading-template').fadeOut('slow');
-        //            $("html, body").animate({
-        //                scrollTop: 0
-        //            }, 1000);
-        //        });
-        //        return false; 
+        $('div.pager').remove(); 
+        $('#message').html("Loading Data");
+        $('#loading-template').show(); 
+        $('#wall_container').empty(); 
+        $('#wall_container').load("<?php echo site_url('forum/wall_content_podcast_year') ?>/"+year,function(){            
+            $('#loading-template').fadeOut('slow');
+            $("html, body").animate({
+                scrollTop: 0
+            }, 1000);
+        });
+        return false; 
     });
 </script>
