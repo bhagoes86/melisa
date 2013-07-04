2e6c10891ca918bc015cd4dda7c6ae34a57b55a0
Taufik Sulaeman P <taufiksu@gmail.com>
2013-07-04 08:50:39 +0700

    widget podcast time

--- application/modules/forum/views/widget_podcast_meta_time.php
+++ application/modules/forum/views/widget_podcast_meta_time.php
@@ -19,7 +19,7 @@
                 <span class="helper">Podcast Tahun Sekarang</span>
             </label><br/>
             <label class="input-control radio fg-color-orangeDark">
-                <input type="radio" name="podcast-year" date-year="<?php echo date('Y') - 1 ?>">
+                <input type="radio" name="podcast-last-year" date-year="<?php echo date('Y') - 1 ?>">
                 <span class="helper">Podcast Tahun Lalu</span>
             </label><br/>
             <label class="input-control radio fg-color-orangeDark">
@@ -31,6 +31,20 @@
 </fieldset>
 <script type="text/javascript">
     $('input[name=podcast-year]:radio').change(function(){
+        var year = $(this).attr('data-year');
+        $('div.pager').remove(); 
+        $('#message').html("Loading Data");
+        $('#loading-template').show(); 
+        $('#wall_container').empty(); 
+        $('#wall_container').load("<?php echo site_url('forum/wall_content_podcast_year') ?>/"+year,function(){            
+            $('#loading-template').fadeOut('slow');
+            $("html, body").animate({
+                scrollTop: 0
+            }, 1000);
+        });
+        return false; 
+    });
+    $('input[name=podcast-last-year]:radio').change(function(){
         var year = $(this).attr('data-year');
         $('div.pager').remove(); 
         $('#message').html("Loading Data");
