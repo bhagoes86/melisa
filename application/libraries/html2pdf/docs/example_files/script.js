a9ac22cee886ac648a7c0794baf83fd78e339f78
taufiksu <taufiksu@gmail.com>
2013-08-28 07:22:05 +0700

    some deleted file

--- application/libraries/html2pdf/docs/example_files/script.js
+++
@@ -1,7 +0,0 @@
-function sondage_pro(obj)
-{
-	if (obj.checked)
-		document.getElementById('span_user_pro').style.display = '';
-	else
-		document.getElementById('span_user_pro').style.display = 'none';
-}
