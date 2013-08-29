f76c81b56ae4ac40955c15b9f4f121ea8d0f9a8b
taufiksu <taufiksu@gmail.com>
2013-08-29 15:52:15 +0700

    loadmore fixed news feed

--- application/modules/mobile/views/feed/list_layout.php
+++ application/modules/mobile/views/feed/list_layout.php
@@ -1,7 +1,7 @@
 <?php foreach ($feed as $rowfeed): ?>
     <li data-cards-type='text'>
-        <!--<h2><?php // echo $rowfeed->first_name . ' ' . $rowfeed->last_name      ?> - <?php echo nicetime(strtotime($rowfeed->date)) ?></h2>-->
-        <!--<div><img src="<?php // echo base_url() . 'resource/' . $row->picture                                                             ?>" style="width: 100%;"/></div>-->
+        <h2><?php echo $rowfeed->first_name . ' ' . $rowfeed->last_name ?> - <?php echo nicetime(strtotime($rowfeed->date)) ?></h2>
+        <!--<div><img src="<?php // echo base_url() . 'resource/' . $row->picture                                                              ?>" style="width: 100%;"/></div>-->
         <a href='#'><i class='icon-screenshot'></i> Navigate</a>
         <p style="align: justify;"><div style="max-width: 100%;text-align: justify;"><?php echo $rowfeed->message ?></div></p>
     </li>
