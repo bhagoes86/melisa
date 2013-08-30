9f1767db7465ca6b6faab95107d0d4719d1bbf00
taufiksu <taufiksu@gmail.com>
2013-08-31 04:15:44 +0700

    eksternal viewer

--- application/modules/mobile/controllers/mobile.php
+++ application/modules/mobile/controllers/mobile.php
@@ -139,7 +139,7 @@ class Mobile extends MX_Controller {
     /*
      * Content
      */
-    
+
     // podcast all new    
     function list_podcast_new() {
         if (!$this->ion_auth->logged_in()) {
@@ -156,15 +156,17 @@ class Mobile extends MX_Controller {
         $data['podcast'] = $this->model_mobile->get_podcast($offset);
         $this->load->view('mobile/content/list_podcast_layout', $data);
     }
-    
+
     function viewer_youtube($id_content) {
+        $data['site'] = $this->model_mobile->select_themes()->row();
         $data['content'] = $this->model_mobile->select_content_by_id($id_content)->row();
-        $this->load->view('mobile/content/viewer_youtube', $data);        
+        $this->load->view('mobile/content/viewer_youtube', $data);
     }
-    
+
     function viewer_soundcloud($id_content) {
+        $data['site'] = $this->model_mobile->select_themes()->row();
         $data['content'] = $this->model_mobile->select_content_by_id($id_content)->row();
-        $this->load->view('mobile/content/viewer_soundcloud', $data);        
+        $this->load->view('mobile/content/viewer_soundcloud', $data);
     }
 
     /*
