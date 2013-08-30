556f7530fa29b1968e939970aec22eb6d3a91ca2
taufiksu <taufiksu@gmail.com>
2013-08-30 19:35:44 +0700

    content cover

--- application/modules/mobile/views/course/list_content_by_syllabus.php
+++ application/modules/mobile/views/course/list_content_by_syllabus.php
@@ -1,6 +1,5 @@
 <?php foreach ($content as $rowcontent): ?>
     <li data-cards-type='text'>
-        <h2><?php echo $rowcontent->title ?></h2>
         <?php if ($rowcontent->type == 0) { ?><!--Video-->
         <?php } elseif ($rowcontent->type == 1) { ?><!--Document-->
         <?php } elseif ($rowcontent->type == 2) { ?><!--Youtube-->
@@ -9,12 +8,14 @@
             //ekstrak the youtube id
             $extract_id = explode('^^^', $media);
             ?>
-            <img src="http://img.youtube.com/vi/<?php echo $extract_id[1]; ?>/1.jpg" style="width: 100%;">
+            <img id="cover_<?php echo $rowcontent->id_content?>" src="http://img.youtube.com/vi/<?php echo $extract_id[1]; ?>/1.jpg" style="width: 100%;">
         <?php } elseif ($rowcontent->type == 3) { ?><!--Vimeo-->
         <?php } elseif ($rowcontent->type == 4) { ?><!--Scribd-->
         <?php } elseif ($rowcontent->type == 5) { ?><!--Slideshare-->
         <?php } elseif ($rowcontent->type == 6) { ?><!--SoundCloud-->
         <?php } elseif ($rowcontent->type == 7) { ?><!--Docstoc-->
         <?php } ?>
+        <div id="content_player" style="display: none;"></div>
+        <h1><?php echo $rowcontent->title ?></h1>
     </li>
 <?php endforeach; ?>
