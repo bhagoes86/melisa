601119eb81d880170a3b1e3619e30754054d2b98
taufiksu <taufiksu@gmail.com>
2013-09-02 06:53:50 +0700

    label document list

--- application/modules/mobile/views/content/list_document.php
+++ application/modules/mobile/views/content/list_document.php
@@ -60,15 +60,17 @@
                     <?php foreach ($document as $rowdocument): ?>
                         <li data-cards-type='text'>
                             <?php if ($rowdocument->type == 1) { ?><!--document-->
-                                <h2><i class="icon-film"></i> Video Content</h2>
+                                <h2>Document Content</h2>
                                 <a href="javascript:void(0)">
                                     <img src="<?php echo base_url() . 'resource/' . $rowdocument->id_content . '.jpg' ?>" style="width: 100%;">
                                 </a>
                             <?php } elseif ($rowdocument->type == 4) { ?><!--scribd-->
+                                <h2>Scribd Content</h2>
                                 <a href="javascript:void(0)" style="text-align: center">
                                     <i class="icon-file-text-alt"></i>
                                 </a>
                             <?php } elseif ($rowdocument->type == 7) { ?><!--docstoc-->
+                                <h2>Docstoc Content</h2>
                                 <?php
                                 $media = analyze_media($rowdocument->file);
                                 $extract_id = explode('^^^', $media);
