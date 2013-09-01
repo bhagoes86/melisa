efb792bb98ce570a8852b7a37920be1eac9ec175
taufiksu <taufiksu@gmail.com>
2013-09-01 08:27:57 +0700

    downloader

--- application/modules/mobile/views/content/viewer_video.php
+++ application/modules/mobile/views/content/viewer_video.php
@@ -9,16 +9,7 @@
         <link rel="stylesheet" href="<?php echo base_url() ?>mobileasset/css/jquerymobile.nativedroid.light.css"  id='jQMnDTheme' />
         <link rel="stylesheet" href="<?php echo base_url() ?>mobileasset/css/jquerymobile.nativedroid.color.green.css" id='jQMnDColor' />
         <script src="<?php echo base_url() ?>mobileasset/js/jquery-1.10.2.min.js"></script>
-        <script src="<?php echo base_url() ?>mobileasset/js/jquery.mobile-1.3.2.min.js"></script>
-        <script src="<?php echo base_url() ?>mobileasset/flowplayer-5.4.3/flowplayer.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>asset/flowplayer/skin/minimalist.css" />
-        <style type="text/css">
-            .flowplayer { background-color: #222; background-size: cover; }
-            .flowplayer .fp-controls { background-color: rgba(0, 0, 0, 0.4)}
-            .flowplayer .fp-timeline { background-color: rgba(0, 0, 0, 0.5)}
-            .flowplayer .fp-progress { background-color: rgba(219, 0, 0, 1)}
-            .flowplayer .fp-buffer { background-color: rgba(249, 249, 249, 1)}
-        </style>
+        <script src="<?php echo base_url() ?>mobileasset/js/jquery.mobile-1.3.2.min.js"></script>        
     </head>
     <body>
         <div data-role="page" id="page" data-theme="d">
@@ -34,28 +25,21 @@
             <?php echo $this->load->view('mobile/content/panel_detail_video'); ?>
             <!--Content-->
             <div data-role="content">
-                <script type="text/javascript">
-                    $(function() {
-                        $("#viewer_video").flowplayer();
-                    });
-                </script>
-                <div id="viewer_video" alt="<?php echo $content->id_content ?>" data-swf="<?php echo base_url() ?>mobileasset/flowplayer-5.4.3/flowplayer.swf" class="flowplayer play-button" style="height:50%;width: 100%;padding: 0px;boder: 0px;" data-ratio="0.5625">
-                    <video alt="<?php echo $content->id_content ?>" style="height:50%;width: 100%;padding: 0px;boder: 0px;">
-                        <?php if ($content->ext == '.mp4') { ?> 
-                            <source type="video/mp4" src="<?php echo base_url() . 'resource/' . $content->id_content . '.mp4'; ?>"/>
-                        <?php } elseif ($content->ext == '.MP4') { ?>
-                            <source type="video/mp4" src="<?php echo base_url() . 'resource/' . $content->id_content . '.MP4'; ?>"/>
-                        <?php } elseif ($content->ext == '.FLV') { ?>
-                            <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.FLV'; ?>"/>
-                        <?php } elseif ($content->ext == '.flv') { ?>
-                            <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.flv'; ?>"/>
-                        <?php } elseif ($content->ext == '.MOV') { ?>
-                            <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.flv'; ?>"/>
-                        <?php } elseif ($content->ext == '.mov') { ?>
-                            <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.flv'; ?>"/>
-                        <?php } ?>
-                    </video>        
-                </div>
+                <video poster="<?php echo base_url() . 'resource' . '/' . $content->cover . '.jpg' ?>" style="height:50%;width: 100%;padding: 0px;border: 0px;">
+                    <?php if ($content->ext == '.mp4') { ?> 
+                        <source type="video/mp4" src="<?php echo base_url() . 'resource/' . $content->id_content . '.mp4'; ?>"/>
+                    <?php } elseif ($content->ext == '.MP4') { ?>
+                        <source type="video/mp4" src="<?php echo base_url() . 'resource/' . $content->id_content . '.MP4'; ?>"/>
+                    <?php } elseif ($content->ext == '.FLV') { ?>
+                        <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.FLV'; ?>"/>
+                    <?php } elseif ($content->ext == '.flv') { ?>
+                        <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.flv'; ?>"/>
+                    <?php } elseif ($content->ext == '.MOV') { ?>
+                        <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.flv'; ?>"/>
+                    <?php } elseif ($content->ext == '.mov') { ?>
+                        <source type="video/flv" src="<?php echo base_url() . 'resource/' . $content->id_content . '.flv'; ?>"/>
+                    <?php } ?>
+                </video>        
             </div>
         </div>
         <script src="<?php echo base_url() ?>mobileasset/js/nativedroid.script.js"></script>
