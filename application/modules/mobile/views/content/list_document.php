a2fe5e30b8433b35c5d623dfb21855469f5c89fb
taufiksu <taufiksu@gmail.com>
2013-09-02 06:52:48 +0700

    document list

--- application/modules/mobile/views/content/list_document.php
+++ application/modules/mobile/views/content/list_document.php
@@ -59,12 +59,23 @@
                 <ul data-nativedroid-plugin='cards' id="main_content">
                     <?php foreach ($document as $rowdocument): ?>
                         <li data-cards-type='text'>
-                            <?php if ($rowdocument->type == 1) { ?><!--Video-->
+                            <?php if ($rowdocument->type == 1) { ?><!--document-->
                                 <h2><i class="icon-film"></i> Video Content</h2>
-                                <a href="<?php echo site_url('mobile/viewer_video' . '/' . $rowdocument->id_content) ?>" data-ajax="false">
-                                    <img id="cover_<?php echo $rowdocument->id_content ?>" src="<?php echo base_url() . 'resource' . '/' . $rowdocument->cover . '.jpg' ?>" style="width: 100%;">
+                                <a href="javascript:void(0)">
+                                    <img src="<?php echo base_url() . 'resource/' . $rowdocument->id_content . '.jpg' ?>" style="width: 100%;">
                                 </a>
-                            <?php } elseif ($rowdocument->type == 2) { ?><!--Youtube-->
+                            <?php } elseif ($rowdocument->type == 4) { ?><!--scribd-->
+                                <a href="javascript:void(0)" style="text-align: center">
+                                    <i class="icon-file-text-alt"></i>
+                                </a>
+                            <?php } elseif ($rowdocument->type == 7) { ?><!--docstoc-->
+                                <?php
+                                $media = analyze_media($rowdocument->file);
+                                $extract_id = explode('^^^', $media);
+                                ?>
+                                <a href="javascript:void(0)">
+                                    <img src="http://img.docstoccdn.com/thumb/100/<?php echo $extract_id[1] ?>.png" style="width: 100%;">
+                                </a>
                             <?php } ?>
                             <p><?php echo $rowdocument->title ?></p>
                         </li>
--- application/modules/mobile/views/content/list_document_layout.php
+++ application/modules/mobile/views/content/list_document_layout.php
@@ -1,7 +1,23 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<?php foreach ($document as $rowdocument): ?>
+    <li data-cards-type='text'>
+        <?php if ($rowdocument->type == 1) { ?><!--document-->
+            <h2><i class="icon-film"></i> Video Content</h2>
+            <a href="javascript:void(0)">
+                <img src="<?php echo base_url() . 'resource/' . $rowdocument->id_content . '.jpg' ?>" style="width: 100%;">
+            </a>
+        <?php } elseif ($rowdocument->type == 4) { ?><!--scribd-->
+            <a href="javascript:void(0)" style="text-align: center">
+                <i class="icon-file-text-alt"></i>
+            </a>
+        <?php } elseif ($rowdocument->type == 7) { ?><!--docstoc-->
+            <?php
+            $media = analyze_media($rowdocument->file);
+            $extract_id = explode('^^^', $media);
+            ?>
+            <a href="javascript:void(0)">
+                <img src="http://img.docstoccdn.com/thumb/100/<?php echo $extract_id[1] ?>.png" style="width: 100%;">
+            </a>
+        <?php } ?>
+        <p><?php echo $rowdocument->title ?></p>
+    </li>
+<?php endforeach; ?>
