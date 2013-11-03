103204204a4d9ccca15fffd2378736c3bd4b897f
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2013-11-04 06:08:56 +0700

    themes

--- application/modules/site/views/footbar.php
+++ application/modules/site/views/footbar.php
@@ -1,5 +1,5 @@
 <div class="nav-bar">
-    <div class="nav-bar-inner padding10" style="background: <?php echo $footbar->bgfooter; ?>;">
+    <div class="nav-bar-inner padding10" style="background: <?php echo '#'.$footbar->bgfooter; ?>;">
         <span class="element">
             <a class="fg-color-white" href="<?php echo site_url(); ?>">&copy; <?php echo date('Y'); ?> <?php echo $footbar->footer; ?></a>, Styled with <a class="fg-color-white" href="http://metroui.org.ua">Metro UI CSS</a>
         </span>
